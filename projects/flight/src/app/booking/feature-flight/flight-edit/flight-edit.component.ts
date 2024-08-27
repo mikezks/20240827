c8d07b67b844c1ba517443062d36bd977eb3dfe1
mikezks <23002661+mikezks@users.noreply.github.com>
2024-08-27 15:17:41 +0200

    feat: typed forms

--- projects/flight/src/app/booking/feature-flight/flight-edit/flight-edit.component.ts
+++ projects/flight/src/app/booking/feature-flight/flight-edit/flight-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnChanges, SimpleChanges, inject } from '@angular/core';
-import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { routerFeature } from '../../../shared/logic-router-state';
 import { initialFlight } from '../../logic-flight';
@@ -20,7 +20,7 @@ export class FlightEditComponent implements OnChanges {
 
   @Input() flight = initialFlight;
 
-  protected editForm = inject(NonNullableFormBuilder).group({
+  protected editForm = inject(FormBuilder).nonNullable.group({
     id: [0],
     from: [''],
     to: [''],
@@ -28,10 +28,18 @@ export class FlightEditComponent implements OnChanges {
     delayed: [false]
   });
 
+  name = new FormControl('Mary', { nonNullable: true});
+
   constructor() {
     this.store.select(routerFeature.selectRouteParams).subscribe(
       params => console.log(params)
     );
+
+    this.editForm.getRawValue(); // Full type, not Partial only.
+    this.editForm.patchValue({
+      from: 'City name'
+      // from: 123                  String is allowed, but no other type.
+    })
   }
 
   ngOnChanges(changes: SimpleChanges): void {
