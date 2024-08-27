aafa5f2be86950d6a166aa4e18d32d74cde6775d
mikezks <23002661+mikezks@users.noreply.github.com>
2024-08-27 12:43:24 +0200

    refactor: router default export

--- projects/flight/src/app/app.config.ts
+++ projects/flight/src/app/app.config.ts
@@ -2,20 +2,21 @@ import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http"
 import { ApplicationConfig, importProvidersFrom } from "@angular/core";
 import { EffectsModule } from "@ngrx/effects";
 import { StoreModule } from "@ngrx/store";
-import { AppRoutingModule } from "./app/app-routing.module";
-import { routerFeature } from "./app/shared/logic-router-state";
-import { SharedModule } from "./app/shared/shared.module";
+import { routerFeature } from "./shared/logic-router-state";
+import { SharedModule } from "./shared/shared.module";
+import { provideRouter } from "@angular/router";
+import { APP_ROUTES } from "./app.routes";
 
 
 export const appConfig: ApplicationConfig = {
   providers: [
     importProvidersFrom(
-      AppRoutingModule,
       StoreModule.forRoot(),
       EffectsModule.forRoot(),
       StoreModule.forFeature(routerFeature),
       SharedModule
     ),
+    provideRouter(APP_ROUTES),
     provideHttpClient(
       withInterceptorsFromDi()
     )
