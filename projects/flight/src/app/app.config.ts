e1dbf55d103617dd6a64b473736df204e1636081
mikezks <23002661+mikezks@users.noreply.github.com>
2024-08-28 15:25:29 +0200

    build: ssr integration

--- projects/flight/src/app/app.config.ts
+++ projects/flight/src/app/app.config.ts
@@ -6,6 +6,7 @@ import { provideStore } from '@ngrx/store';
 import { APP_ROUTES } from './app.routes';
 import { provideRouterFeature } from './shared/logic-router-state';
 import { provideConfigState } from './shared/util-config';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -20,6 +21,9 @@ export const appConfig: ApplicationConfig = {
     provideStore(),
     provideEffects(),
     provideRouterFeature(),
-    provideConfigState('./config.state.json')
+    provideConfigState('./config.state.json'),
+    provideClientHydration(
+      withEventReplay()
+    )
   ]
 };
