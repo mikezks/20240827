eddc3d46f7c7d4296cee7f2129912ef2a30c24c7
mikezks <23002661+mikezks@users.noreply.github.com>
2024-08-28 10:39:44 +0200

    refactor: passenger search signal demo

--- projects/flight/src/app/checkin/feature-passenger/passenger-search/passenger-search.component.ts
+++ projects/flight/src/app/checkin/feature-passenger/passenger-search/passenger-search.component.ts
@@ -1,5 +1,5 @@
 import { NgFor, NgIf } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, computed, effect, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { PassengerStore, Passenger } from '../../logic-passenger';
@@ -18,17 +18,47 @@ import { PassengerStore, Passenger } from '../../logic-passenger';
 export class PassengerSearchComponent {
   private store = inject(PassengerStore);
 
-  firstname = '';
-  lastname = 'Smith';
+  firstname = signal('');
+  lastname = signal('Smith');
   passengers = this.store.passengerEntities;
   selectedPassenger?: Passenger;
+  username = computed(
+    () => this.firstname().toLowerCase() + '.' + this.lastname().toLowerCase()
+  );
+
+  constructor() {
+    effect(() => console.log(this.username()));
+
+    console.log(this.username())
+    this.firstname.set('Peter');
+    console.log(this.username())
+    this.firstname.set('Mary');
+    console.log(this.username())
+    this.firstname.set('Anne');
+    console.log(this.username())
+    this.firstname.set('Andy');
+    console.log(this.username())
+    this.firstname.set('Sue');
+    console.log(this.username());
+
+    // Glitch free
+    const counter = signal(0);
+    const isEven = computed(
+      () => counter() % 2 === 0 ? true : false
+    );
+    // counter.set(1)
+    // counter: 1
+    // isEven: true   -> not emitted by the computed signal
+    // isEven: false
+
+  }
 
   search(): void {
     if (!(this.firstname || this.lastname)) return;
 
     this.store.loadPassengers({
-      firstName: this.firstname,
-      name: this.lastname
+      firstName: this.firstname(),
+      name: this.lastname()
     });
   }
 
